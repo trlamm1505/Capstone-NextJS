a33677e6aea83c7e3915777b8186ade2295e4285
Thai <nhikute987654321@gmail.com>
2025-10-05 20:57:47 +0700

    update service

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,12 +6,21 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
     const tokenCybersoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgNTIiLCJIZXRIYW5TdHJpbmciOiIyNy8wNC8yMDI2IiwiSGV0SGFuVGltZSI6IjE3NzcyNDgwMDAwMDAiLCJuYmYiOjE3NTg5MDk2MDAsImV4cCI6MTc3NzM5OTIwMH0._b9cEhCuhW5AQ7TsywHkbc2NkdJDSmQZYCxkjTSbv3I";
+    const accessToken = localStorage.getItem('accessToken');
 
     if (!config.headers) config.headers = {} as any;
     if (config.headers instanceof AxiosHeaders) {
         config.headers.set("tokenCybersoft", tokenCybersoft);
+        if (accessToken) {
+            config.headers.set("Authorization", `Bearer ${accessToken}`);
+            config.headers.set("token", `${accessToken}`);
+        }
     } else {
         (config.headers as any)["tokenCybersoft"] = tokenCybersoft;
+        if (accessToken) {
+            (config.headers as any)["Authorization"] =`Bearer ${accessToken}`;
+            (config.headers as any)["token"] = `${accessToken}`;
+        }
     }
 
     (config.headers as any)["Cache-Control"] = "no-cache";
